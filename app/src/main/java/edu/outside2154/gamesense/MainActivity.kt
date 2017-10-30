245ff52881d85d0fae100f43fd553bd8afb14e32
sghariha <sghariha@gmail.com>
2017-10-29 17:35:57 -0700

    Added Firebase to the project.

--- app/src/main/java/edu/outside2154/gamesense/MainActivity.kt
+++ app/src/main/java/edu/outside2154/gamesense/MainActivity.kt
@@ -2,11 +2,25 @@ package edu.outside2154.gamesense
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testButton = findViewById<Button>(R.id.firebase_test_button)
+        val db = FirebaseDatabase.getInstance()
+
+        testButton.setOnClickListener { _ ->
+            val myRef = db.getReference("test")
+            myRef.setValue("working!")
+        }
     }
 }
