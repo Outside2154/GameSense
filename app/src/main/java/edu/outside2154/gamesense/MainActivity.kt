b46ab8064cb130b78b4b4777a29258652bdf7c66
MTD <m9duong@ucsd.edu>
2017-11-04 13:37:38 -0700

    two images added

--- app/src/main/java/edu/outside2154/gamesense/MainActivity.kt
+++ app/src/main/java/edu/outside2154/gamesense/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         val latest = ExtraSensory(applicationContext).users?.first()?.files?.first()?.info
         val hw = findViewById<TextView>(R.id.es_debug)
         hw.text = latest.toString()
+
     }
 }
 
