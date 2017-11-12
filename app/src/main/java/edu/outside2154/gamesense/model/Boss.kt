588486f62606710f557c5136c84da9e90f889b14
nurbergen-light <32476129+nurbergen-light@users.noreply.github.com>
2017-11-12 14:33:33 -0800

    Add Stat, Boss, Character Classes (#11)
    * Stat Class
    
    Implemented the Stat Class.
    
    * Boss, Character and Stat classes
    
    * JDK Version Change
    
    * JDK Version Change
    
    * Refined Stat, Boss, Character Classes
    
    * remove .DS_Store files
    
    * Refined Classes

---
+++ app/src/main/java/edu/outside2154/gamesense/model/Boss.kt
@@ -0,0 +1,27 @@
+package edu.outside2154.gamesense.model
+
+const val BOSS_BASE_HEALTH = 100.0
+const val BOSS_HEALTH_INC = 50.0
+const val BOSS_BASE_ATTACK = 20.0
+const val BOSS_ATTACK_INC = 5.0
+
+class Boss {
+    var health = BOSS_BASE_HEALTH
+        private set
+    var attack = BOSS_BASE_ATTACK
+        private set
+    var lvl = 1
+        private set
+    val dead
+        get() = health == 0.0
+
+    fun takeDamage(damage: Double){
+        health = maxOf(health - damage, 0.0)
+    }
+
+    fun reset(userWon: Boolean) {
+        if (userWon) lvl++
+        health = BOSS_BASE_HEALTH + BOSS_HEALTH_INC * (lvl - 1)
+        attack = BOSS_BASE_ATTACK + BOSS_ATTACK_INC * (lvl - 1)
+    }
+}
---
+++ app/src/main/java/edu/outside2154/gamesense/model/Character.kt
@@ -0,0 +1,46 @@
+package edu.outside2154.gamesense.model
+
+import java.util.*
+
+const val USER_BASE_HEALTH = 100.0
+const val USER_BASE_ATTACK = 100.0
+const val USER_CRIT_MULT = 2.0
+
+
+class Character (_intStat: Stat, _atkStat: Stat, _regenStat: Stat) {
+    var health = USER_BASE_HEALTH
+        private set
+    var intStat = _intStat
+        private set
+    var atkStat = _atkStat
+        private set
+    var regenStat = _regenStat
+        private set
+    var currency = 0.0
+        private set
+    val dead
+        get() = health == 0.0
+    private val rand = Random()
+
+    private fun isCritical(): Boolean{
+        return rand.nextDouble() < intStat.calcStat() ?: 0.0
+    }
+
+    private fun takeDamage(damage: Double){
+        health = maxOf(health - damage, 0.0)
+    }
+
+    fun fightWith(boss: Boss){
+        if (boss.dead) return
+
+        // User attacks
+        val attack = atkStat.calcStat() ?: 0.0
+        var damage = attack * USER_BASE_ATTACK
+        if (isCritical()) damage *= USER_CRIT_MULT
+        boss.takeDamage(damage)
+
+        // Boss attacks
+        if (boss.dead) return
+        takeDamage(boss.attack)
+    }
+}
