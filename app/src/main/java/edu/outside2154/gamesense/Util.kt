4e7f72806b2255b988e7e6d7a7c934dded27b9fd
Ethan Chan <metakirby5@gmail.com>
2017-11-13 00:16:25 -0800

    add docstrings, fix a variable name

--- app/src/main/java/edu/outside2154/gamesense/Util.kt
+++ app/src/main/java/edu/outside2154/gamesense/Util.kt
@@ -6,13 +6,23 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.util.TypedValue
 
+/**
+ * Runs [func] with a [FragmentManager] as receiver, automatically handling
+ * [FragmentManager.beginTransaction] and [FragmentTransaction.commit].
+ */
 inline fun FragmentManager.transact(func: FragmentTransaction.() -> FragmentTransaction) =
         beginTransaction().func().commit()
 
+/**
+ * Runs [func] with a [TypedArray] as receiver, automatically handling [TypedArray.recycle].
+ */
 inline fun TypedArray.runAndRecycle(func: TypedArray.() -> Unit) =
         try { func() } finally { recycle() }
 
-fun dpToPx(context: Context, px: Double): Int =
+/**
+ * Converts [dp] from display units to pixels.
+ */
+fun dpToPx(context: Context, dp: Double): Int =
         TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP, px.toFloat(),
+                TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(),
                 context.resources.displayMetrics).toInt()
