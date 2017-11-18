5cac2f3a8555ae81c46493d588590650203ab91e
Ethan Chan <metakirby5@gmail.com>
2017-11-18 13:52:25 -0800

    refactor to put state variables in NavActivity (#21)
    Details:
    - Add `FromFirebase` and `FromFirebaseAndUpdate` property delegates to
      represent properties to be constructed from firebase nodes
    - Add `Updatable` and `BundleUpdatable` interfaces to facilitate
      updating on hearing back from Firebase
    - Use synthetic properties where possible
    - Remove cruft from `Fragment` template
    - Rename `util/Util.kt` to `util/Android.kt` so `util/Util.kt` can be
      used for general, non Android-specific utilities

--- app/src/main/java/edu/outside2154/gamesense/fragment/HomeFragment.kt
+++ app/src/main/java/edu/outside2154/gamesense/fragment/HomeFragment.kt
@@ -8,9 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 
 import edu.outside2154.gamesense.R
-import edu.outside2154.gamesense.activity.NavActivity
-import edu.outside2154.gamesense.database.firebaseListen
 import edu.outside2154.gamesense.model.*
+import edu.outside2154.gamesense.util.BundleUpdatable
+import edu.outside2154.gamesense.util.Updatable
+import edu.outside2154.gamesense.util.toIntPercent
 import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
@@ -21,119 +22,41 @@ import kotlinx.android.synthetic.main.fragment_home.*
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
-
-    // TODO: Rename and change types of parameters
-    private var mParam1: String? = null
-    private var mParam2: String? = null
-
-    private var mListener: OnFragmentInteractionListener? = null
-
+class HomeFragment : Fragment(), Updatable, BundleUpdatable {
     private var player: Player? = null
     private var boss: Boss? = null
-    private lateinit var androidId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Retrieve items from bundle.
-        arguments.run {
-            androidId = getString("androidId")
-            mParam1 = getString(ARG_PARAM1)
-            mParam2 = getString(ARG_PARAM2)
-            player = getSerializable("player") as Player?
-            boss = getSerializable("boss") as Boss?
-        }
-
-        if (player == null) {
-            firebaseListen(androidId) {
-                player = PlayerFirebaseImpl(it)
-                updatePlayerBars()
-                (activity as NavActivity).player = player
-            }
-        }
-
-        // If we didn't have a boss, get it from Firebase.
-        // Once we have the boss, update.
-        if (boss == null) {
-            firebaseListen("$androidId/boss") {
-                boss = BossFirebaseImpl(it)
-                updateBossBar()
-                (activity as NavActivity).boss = boss
-            }
-        }
+        updateBundle(arguments)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+                              savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_home, container, false)
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        updatePlayerBars()
-        updateBossBar()
+        update()
     }
 
-    private fun updatePlayerBars() {
-        player?.let {
-            hp_lb.progress = it.health.toInt()
-            atk_lb.progress = ((it.atkStat.calcStat() ?: 0.0) * 100).toInt()
-            int_lb.progress = ((it.intStat.calcStat() ?: 0.0) * 100).toInt()
+    override fun updateBundle(bundle: Bundle) {
+        bundle.run {
+            player = getSerializable("player") as Player?
+            boss = getSerializable("boss") as Boss?
         }
     }
 
-    private fun updateBossBar() {
+    override fun update() {
+        // Update all progress bars
+        player?.let {
+            hp_lb.progress = it.health.toInt()
+            atk_lb.progress = it.atkStat.calcStat()?.toIntPercent() ?: 0
+            int_lb.progress = it.intStat.calcStat()?.toIntPercent() ?: 0
+        }
+
         boss?.let {
             boss_hp_lb.progress = it.health.toInt()
         }
     }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) = mListener?.onFragmentInteraction(uri)
-
-
-    override fun onDetach() {
-        super.onDetach()
-        mListener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        fun newInstance(param1: String, param2: String): HomeFragment {
-            val fragment = HomeFragment()
-            fragment.arguments = Bundle().apply {
-                putString(ARG_PARAM1, param1)
-                putString(ARG_PARAM2, param2)
-            }
-            return fragment
-        }
-    }
-}// Required empty public constructor
+}
--- app/src/main/java/edu/outside2154/gamesense/util/Util.kt
+++ app/src/main/java/edu/outside2154/gamesense/util/Util.kt
@@ -1,55 +1,18 @@
 package edu.outside2154.gamesense.util
 
-import android.content.res.TypedArray
-import android.os.Build
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentTransaction
-import android.util.TypedValue
-import android.content.Context
-import android.provider.Settings.Secure
+/**
+ * Represents an object which requires updating upon some state change.
+ */
+interface Updatable {
+    fun update()
+}
 
 /**
- * Runs [func] with a [FragmentManager] as receiver, automatically handling
- * [FragmentManager.beginTransaction] and [FragmentTransaction.commit].
+ * Turns a double in the range [0.0, 1.0] to an integer in the range [0, 100].
+ * @return The converted double if the value is in range, otherwise null.
  */
-inline fun FragmentManager.transact(func: FragmentTransaction.() -> FragmentTransaction) =
-        beginTransaction().func().commit()
-
-/**
- * Runs [func] with a [TypedArray] as receiver, automatically handling [TypedArray.recycle].
- */
-inline fun TypedArray.runAndRecycle(func: TypedArray.() -> Unit) =
-        try { func() } finally { recycle() }
-
-/**
- * Converts [dp] from display units to pixels.
- */
-fun dpToPx(context: Context, dp: Double): Int =
-        TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(),
-                context.resources.displayMetrics).toInt()
-
-/**
- * Returns Android ID of current device
- *
- * @param Takes context in to determine ID
- * @return Returns default Android ID if an emulator is being used, or the actual Android device ID
- */
-fun getAndroidId(context: Context): String =
-    if (isEmulator()) "1cf08e3503018df0" else Secure.getString(context.contentResolver, Secure.ANDROID_ID)
-
-/**
- * Handles determining if currently running device is an emulator for testing purposes
- *
- * @return Boolean of whether or not the current running device is an emulator
- */
-fun isEmulator(): Boolean {
-    return (Build.FINGERPRINT.startsWith("generic")
-            || Build.FINGERPRINT.startsWith("unknown")
-            || Build.MODEL.contains("google_sdk")
-            || Build.MODEL.contains("Emulator")
-            || Build.MODEL.contains("Android SDK built for x86")
-            || Build.MANUFACTURER.contains("Genymotion")
-            || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
-            || "google_sdk" == Build.PRODUCT)
+fun Double.toIntPercent(): Int? = when {
+    this < 0.0 -> null
+    this > 1.0 -> null
+    else -> (this * 100).toInt()
 }
