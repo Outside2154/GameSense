cc15450ce81ffe5d7705d180a85110ad1e94c800
Michelle Au <iammichelleau@hotmail.com>
2017-11-19 21:20:24 -0800

    Notifications fragment now pulls notifications from Firebase.

--- app/src/main/java/edu/outside2154/gamesense/fragment/NotificationsFragment.kt
+++ app/src/main/java/edu/outside2154/gamesense/fragment/NotificationsFragment.kt
@@ -6,12 +6,21 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import edu.outside2154.gamesense.R
+import edu.outside2154.gamesense.model.Boss
+import edu.outside2154.gamesense.model.Notification
+import edu.outside2154.gamesense.model.Notifications
+import edu.outside2154.gamesense.model.Player
+import edu.outside2154.gamesense.util.BundleUpdatable
+import edu.outside2154.gamesense.util.Updatable
 import kotlinx.android.synthetic.main.fragment_notifications.*
+import kotlinx.android.synthetic.main.fragment_notifications.view.*
+import kotlinx.android.synthetic.main.fragment_notifications_row.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,19 +30,21 @@ import kotlinx.android.synthetic.main.fragment_notifications.*
  * Use the [NotificationsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class NotificationsFragment : Fragment() {
+class NotificationsFragment : Fragment(), Updatable, BundleUpdatable {
     // TODO: Rename and change types of parameters
-    private var mParam1: String? = null
-    private var mParam2: String? = null
+    private var notificationCount: Int? = null
+    private var messages: Notifications? = null
+    private var strings = mutableListOf<String>()
 
-    private var mListener: NotificationsFragment.OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.run {
-            mParam1 = getString(NotificationsFragment.ARG_PARAM1)
-            mParam2 = getString(NotificationsFragment.ARG_PARAM2)
-        }
+        updateBundle(arguments)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        update()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -42,57 +53,33 @@ class NotificationsFragment : Fragment() {
         val v = inflater.inflate(R.layout.fragment_notifications, container, false)
 
         val notifications = v.findViewById<RecyclerView>(R.id.recycler_view) as RecyclerView
-        val messages = mutableListOf("Welcome to GameSense!", "Let's begin your battle!",
-                "Boss is attacking you!")
 
         notifications.layoutManager = LinearLayoutManager(context)
-        notifications.adapter = NotificationsAdapter(messages)
+        notifications.adapter = NotificationsAdapter(strings)
         return v
     }
 
-    fun onButtonPressed(uri: Uri) = mListener?.onFragmentInteraction(uri)
+    override fun updateBundle(bundle: Bundle) {
+        bundle.run {
+            notificationCount = getSerializable("notificationCount") as Int?
+            messages = getSerializable("notifications") as Notifications?
 
-    override fun onDetach() {
-        super.onDetach()
-        mListener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NotificationsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        fun newInstance(param1: String, param2: String): NotificationsFragment {
-            val fragment = NotificationsFragment()
-            fragment.arguments = Bundle().apply {
-                putString(ARG_PARAM1, param1)
-                putString(ARG_PARAM2, param2)
-            }
-            return fragment
         }
     }
+
+    override fun update() {
+        notificationCount?.let {
+
+        }
+
+        messages?.let {
+            val list: MutableList<Notification> = it.notifications
+
+            for (item in list) {
+                if (item.message.isNotEmpty())
+                    strings.add(item.message)
+            }
+        }
+    }
+
 }
